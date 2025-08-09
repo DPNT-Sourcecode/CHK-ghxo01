4bd11fdeffa030fb10ecde6f922394ed9b81ceeb
sbx_user1051 <sbx_user1051@169.254.51.237>
2025-08-09 19:23:11 +0000

    Sat Aug 09 19:23:11 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -13,6 +13,7 @@ class CheckoutSolution:
         total = items["C"]*20 + items["D"]*15 +(items["A"]//3)*130 + (items["A"]%3)*50 + (items["B"]//2)*45 + (items["B"]%2)*30
         return total
 
+# 1
 
 #+------+-------+----------------+
 #| Item | Price | Special offers |
@@ -22,3 +23,15 @@ class CheckoutSolution:
 #| C    | 20    |                |
 #| D    | 15    |                |
 #+------+-------+----------------+
+
+# 2
+
+#+------+-------+------------------------+
+#| Item | Price | Special offers         |
+#+------+-------+------------------------+
+#| A    | 50    | 3A for 130, 5A for 200 |
+#| B    | 30    | 2B for 45              |
+#| C    | 20    |                        |
+#| D    | 15    |                        |
+#| E    | 40    | 2E get one B free      |
+#+------+-------+------------------------+
