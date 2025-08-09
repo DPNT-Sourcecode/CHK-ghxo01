19d46c1aea2ed1c768785ede37cf5fb3aa50ff55
sbx_user1051 <sbx_user1051@169.254.51.237>
2025-08-09 19:20:01 +0000

    Sat Aug 09 19:20:01 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,5 +3,22 @@ class CheckoutSolution:
 
     # skus = unicode string
     def checkout(self, skus):
-        raise NotImplementedError()
+        items = {"A":0, "B":0, "C":0, "D":0}
+        for char in skus:
+            if char == "A" or char == "B" or char == "C" or char == "D":
+                items[char] += 1
+            else:
+                return -1
         
+        total = items["C"]*20 + items["D"]*15 +(items["A"]//3)*130 + (items["A"]%3)*50 + (items["B"]//2)*45 + (items["B"]%2)*30
+        return total
+
+
+#+------+-------+----------------+
+#| Item | Price | Special offers |
+#+------+-------+----------------+
+#| A    | 50    | 3A for 130     |
+#| B    | 30    | 2B for 45      |
+#| C    | 20    |                |
+#| D    | 15    |                |
+#+------+-------+----------------+
