22d81a881411d94939bc9aab0d57d084cdc8a0f9
sbx_user1051 <sbx_user1051@169.254.51.237>
2025-08-09 19:38:22 +0000

    Sat Aug 09 19:38:22 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,14 +3,12 @@ class CheckoutSolution:
 
     # skus = unicode string
     def checkout(self, skus):
-        items = {"A":0, "B":0, "C":0, "D":0, "E":0}
+        items = {"A":0, "B":0, "C":0, "D":0, "E":0, "F":0}
         for char in skus:
-            if char == "A" or char == "B" or char == "C" or char == "D" or char == "E":
+            if char in "ABCDEF":
                 items[char] += 1
             else:
                 return -1
-        
-        items["B"] = max(0, items["B"] - (items["E"]//2))
 
         A_remaining = items["A"]
         A_multi5 = A_remaining//5
@@ -19,7 +17,18 @@ class CheckoutSolution:
         A_remaining %= 3
         A_cost = (A_multi5 * 200) + (A_multi3 * 130) + (A_remaining * 50)
         
-        total = items["C"]*20 + items["D"]*15 + (items["B"]//2)*45 + (items["B"]%2)*30 + A_cost + items["E"]*40
+        items["B"] = max(0, items["B"] - (items["E"]//2))
+        B_cost = (items["B"]//2)*45 + (items["B"]%2)*30
+
+        C_cost = items["C"]*20
+
+        D_cost = items["D"]*15
+
+        E_cost = items["E"]*40
+
+        F_cost = (items["F"]//3 * 2 * 10) + (items["F"]%3 * 10)
+        
+        total = A_cost + B_cost + C_cost + D_cost + E_cost + F_cost
         return total
 
 # 1
@@ -48,3 +57,18 @@ class CheckoutSolution:
 #+------+-------+------------------------+
 
 # Need to calculate for get one B free, so -1
+
+# 3
+
+#+------+-------+------------------------+
+#| Item | Price | Special offers         |
+#+------+-------+------------------------+
+#| A    | 50    | 3A for 130, 5A for 200 |
+#| B    | 30    | 2B for 45              |
+#| C    | 20    |                        |
+#| D    | 15    |                        |
+#| E    | 40    | 2E get one B free      |
+#| F    | 10    | 2F get one F free      |
+#+------+-------+------------------------+
+
+# Now with F, get one F for free. So that means F//3 * 2 for cost, also time to refactor code
